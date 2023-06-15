9e433ddc9a12352f1f786565aadf3a424ec74c3a
salimdeerhold <samaharjan@deerhold.com>
2023-06-15 13:21:53 +0545

    mutator used for Student

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -14,9 +14,20 @@ class Student extends Model
 
     protected $fillable = ['fullName', 'dob', 'address'];
 
+
+    // mutator i.e. before storing in a tabel fullName 'harry kc' is transformed to 'Harry Kc'
+    public function setFullNameAttribute($value){
+        $this->attributes['fullName'] = ucwords($value);
+    }
+    // mutator
+    public function setAddressAttribute($value){
+        $this->attributes['address'] = ucwords($value);
+    }
+
+
+    // relationship
     public function gradeModels(){
         return $this->hasMany(Grade::class);
     }
-
 
 }
